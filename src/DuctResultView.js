be37d9cb42e2b8cfecd31d2c672af6f6de4ef0bc
E02 <chewjog99@gmail.com>
2024-02-01 19:38:46 +0900

    2024:02:01 작업

--- src/DuctResultView.js
+++ src/DuctResultView.js
@@ -1,13 +1,13 @@
 const DuctResultView = (props) => {
-    const { windVolume, firstH, firstW, firstP, firstV } = props.State;
+    const { windVolume, firstH, firstW, firstP, firstV ,firstD,ducts} = props.State;
 
     return (
         <div className="DiaryList">
             <h2>Result</h2>
             <div>
                 <div>{windVolume}</div>
-                <div>{firstH}</div>
-                <div>{firstW}</div>
+                <div>{ducts === "원형덕트" ? "" : firstH}</div>
+                <div>{ducts === "원형덕트" ? firstD : firstW}</div>
                 <div>{firstP}</div>
                 <div>{firstV}</div>
             </div>
